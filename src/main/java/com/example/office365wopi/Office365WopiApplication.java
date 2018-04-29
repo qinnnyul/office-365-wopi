3cc60a6a6766b828c1820b43aa6424b999829e7f
Yulin Qin <yulin.qin.ext@gmail.com>
2018-04-29 15:46:30 +0800

    add init prjoect based on spring boot

---
+++ src/main/java/com/example/office365wopi/Office365WopiApplication.java
@@ -0,0 +1,12 @@
+package com.example.office365wopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Office365WopiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Office365WopiApplication.class, args);
+	}
+}
