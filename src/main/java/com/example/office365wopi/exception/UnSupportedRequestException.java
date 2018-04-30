36620dc579bd53aeadeaa0ecd9b6447f79f73dc0
Yulin Qin <yulin.qin.ext@gmail.com>
2018-04-30 11:30:56 +0800

    set right http status for "not implemented" request

--- src/main/java/com/example/office365wopi/exception/UnSupportedRequestException.java
+++ src/main/java/com/example/office365wopi/exception/UnSupportedRequestException.java
@@ -1,5 +1,9 @@
 package com.example.office365wopi.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_IMPLEMENTED, reason = "Operation not supported")
 public class UnSupportedRequestException extends RuntimeException {
     public UnSupportedRequestException(String message) {
         super(message);
